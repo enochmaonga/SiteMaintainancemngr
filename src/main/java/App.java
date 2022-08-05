7b9ee81e3ded9151292fe75583ed1df5c00fe85b
Administrator <enoch.maonga@gmail.com>
2022-08-05 14:00:56 +0300

    initial commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,16 @@
 public class App {
+        private String name;
+        private String description;
+        private int id;
 
-}
+        public App(String name, String description) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+    }
+
+
